482dcd9c98c2d594d0e744ff24e438bd620e3a29
김성재 <103095432+seongjae6751@users.noreply.github.com>
2023-11-04 21:01:08 +0900

    mvc 수정

---
+++ src/controller/AgeCalculatorController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import model.AgeCalculator;
+import model.User;
+import model.UserRepository;
+import view.InputView;
+import view.OutputView;
+
+public class AgeCalculatorController {
+    public static void main(String[] args) {
+        AgeCalculator ageCalculator = new AgeCalculator();
+        UserRepository userRepository = new UserRepository();
+        InputView inPutView = new InputView();
+        OutputView outPutView = new OutputView();
+
+        int inputId = inPutView.getInputId();
+        User user = userRepository.findById(inputId);
+
+        int age = ageCalculator.getAge(user.getBirth());
+        boolean isBirthDay = ageCalculator.isBirthDay(user.getBirth());
+        boolean isUnderAge = ageCalculator.isUnderAge(user.getBirth());
+
+        outPutView.displayUserInfo(user.getName(), age, isBirthDay, isUnderAge);
+    }
+}
---
+++ src/view/OutputView.java
@@ -0,0 +1,15 @@
+package view;
+
+public class OutputView {
+    public void displayUserInfo(String name, int age, boolean isBirthDay, boolean isUnderAge) {
+        System.out.println(name + "(" + age + ")");
+        if (isBirthDay) {
+            System.out.printf("%d번째 생일을 축하합니다.\n", age);
+        }
+        if (isUnderAge) {
+            System.out.println("미성년자입니다.");
+        } else {
+            System.out.println("성인입니다.");
+        }
+    }
+}
