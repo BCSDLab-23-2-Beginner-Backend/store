97bd7493407a80f6a84d3d734fb1bbaa7df1610d
U-Jae <77143213+Bungduck@users.noreply.github.com>
2023-11-05 00:55:48 +0900

    refactor: OutputView 분리
    user 데이터, 각종 텍스트 출력 메소드 집합.

--- src/view/OutputView.java
+++ src/view/OutputView.java
@@ -1,4 +1,24 @@
 package view;
 
 public class OutputView {
+
+    public void inputView(){
+        System.out.print("아이디를 입력하세요 : ");
+    }
+
+    public void infoView(String name, int age){
+        System.out.println(name + "(" + age + ")");
+    }
+
+    public void birthdayView(int age){
+        System.out.println(age + "번째 생일을 축하합니다.");
+    }
+
+    public void underAgeView(){
+        System.out.println("미성년자입니다.");
+    }
+
+    public void upperAgeView(){
+        System.out.println("성인입니다.");
+    }
 }
