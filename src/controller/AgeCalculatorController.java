bfe06e91e5576d2146f09fe05e4ad230c88881b7
U-Jae <77143213+Bungduck@users.noreply.github.com>
2023-11-05 01:01:53 +0900

    refactor: AgeCalculatorController 분리
    model의 데이터 조건에 따라 view를 출력하도록 하는 controller 부분입니다.

--- src/controller/AgeCalculatorController.java
+++ src/controller/AgeCalculatorController.java
@@ -1,4 +1,21 @@
 package controller;
+import model.*;
+import view.*;
 
 public class AgeCalculatorController {
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private final AgeCalculator ageCalculator = new AgeCalculator();
+    private final UserRepository userRepository = new UserRepository();
+
+    public void action(){
+        outputView.inputView();
+        User user = userRepository.findById(inputView.getId());
+        int age = ageCalculator.getAge(user.getBirth());
+
+        outputView.infoView(user.getName(), age);
+        if(ageCalculator.isBirthDay(user.getBirth())) outputView.birthdayView(age);
+        if(ageCalculator.isUnderAge(user.getBirth())) outputView.underAgeView();
+        else outputView.upperAgeView();
+    }
 }
