996e08231a96221eb37562d9d3b687a5118e64b5
JuYeong17 <dhwndud980@gmail.com>
2023-11-05 22:24:25 +0900

    model

--- src/Model/AgeCalculator.java
+++ src/Model/AgeCalculator.java
@@ -1,2 +1,23 @@
-package Model;public class AgeCalculator {
+package Model;
+
+import java.time.LocalDate;
+
+public class AgeCalculator {
+    public boolean isUnderAge(LocalDate birth) {
+        int age = getAge(birth);
+        return age <= 19;
+    }
+
+    public int getAge(LocalDate birth) {
+        LocalDate now = LocalDate.now();
+
+        int age = now.getYear() - birth.getYear() + 1;
+        return age;
+    }
+
+    public boolean isBirthDay(LocalDate birth) {
+        LocalDate now = LocalDate.now();
+        return now.getMonthValue() == birth.getMonthValue() && now.getDayOfMonth() == birth.getDayOfMonth();
+    }
 }
+
--- src/Model/User.java
+++ src/Model/User.java
@@ -1,2 +1,21 @@
-package Model;public class User {
+package Model;
+
+import java.time.LocalDate;
+
+public class User {
+    private String name;
+    private LocalDate birth;
+
+    public User(String name, LocalDate birth) {
+        this.name = name;
+        this.birth = birth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirth() {
+        return birth;
+    }
 }
